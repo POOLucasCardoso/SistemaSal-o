13eb97990e6642fe6179777ca47f6705589c4d1d
Lucas Cardoso dos Santos <31657716+POOLucasCardoso@users.noreply.github.com>
2017-09-19 20:44:10 -0300

    Add files via upload

---
+++ Pessoa.java
@@ -0,0 +1,78 @@
+package POO;
+
+public abstract class Pessoa {
+
+	public Contato contatos;
+	
+	public Endereco endereco;
+	
+	public String nome;
+	
+	public String CPF;
+
+	public Pessoa(Contato contatos, Endereco endereco, String nome, String cPF) {
+		super();
+		this.contatos = contatos;
+		this.endereco = endereco;
+		this.nome = nome;
+		CPF = cPF;
+	}
+
+	public Contato getContatos() {
+		return contatos;
+	}
+
+	public void setContatos(Contato contatos) {
+		this.contatos = contatos;
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCPF() {
+		return CPF;
+	}
+
+	public void setCPF(String cPF) {
+		CPF = cPF;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((CPF == null) ? 0 : CPF.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pessoa other = (Pessoa) obj;
+		if (CPF == null) {
+			if (other.CPF != null)
+				return false;
+		} else if (!CPF.equals(other.CPF))
+			return false;
+		return true;
+	}
+
+}
