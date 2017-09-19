20c3a6733fbcb6c00bc11bd92ba4429f9213db30
Lucas Cardoso dos Santos <31657716+POOLucasCardoso@users.noreply.github.com>
2017-09-19 20:09:19 -0300

    Add files via upload

---
+++ Contato.java
@@ -0,0 +1,65 @@
+package POO;
+
+public class Contato {
+
+	private String email;
+	
+	private String numero;
+	
+	public Contato(String email,String numero) {
+
+		this.email=email;
+		this.numero=numero;
+		
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((numero == null) ? 0 : numero.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Contato other = (Contato) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (numero == null) {
+			if (other.numero != null)
+				return false;
+		} else if (!numero.equals(other.numero))
+			return false;
+		return true;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	
+	
+
+}
---
+++ Endereco.java
@@ -0,0 +1,105 @@
+package POO;
+
+public class Endereco {
+
+	private String pais;
+	private String estado;
+	private String cidade;
+	private String bairo;
+	private String rua;
+	private String numero;
+	private String CEP;
+	public Endereco(String pais, String estado, String cidade,String bairo, String rua, String numero, String cEP) {
+		super();
+		this.pais = pais;
+		this.estado = estado;
+		this.cidade = cidade;
+		this.bairo = bairo;
+		this.rua = rua;
+		this.numero = numero;
+		CEP = cEP;
+	}
+	public String getPais() {
+		return pais;
+	}
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getRua() {
+		return rua;
+	}
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public String getCEP() {
+		return CEP;
+	}
+	public void setCEP(String cEP) {
+		CEP = cEP;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((CEP == null) ? 0 : CEP.hashCode());
+		result = prime * result + ((bairo == null) ? 0 : bairo.hashCode());
+		result = prime * result + ((numero == null) ? 0 : numero.hashCode());
+		result = prime * result + ((rua == null) ? 0 : rua.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Endereco other = (Endereco) obj;
+		if (CEP == null) {
+			if (other.CEP != null)
+				return false;
+		} else if (!CEP.equals(other.CEP))
+			return false;
+		if (bairo == null) {
+			if (other.bairo != null)
+				return false;
+		} else if (!bairo.equals(other.bairo))
+			return false;
+		if (numero == null) {
+			if (other.numero != null)
+				return false;
+		} else if (!numero.equals(other.numero))
+			return false;
+		if (rua == null) {
+			if (other.rua != null)
+				return false;
+		} else if (!rua.equals(other.rua))
+			return false;
+		return true;
+	}
+	public String getBairo() {
+		return bairo;
+	}
+	public void setBairo(String bairo) {
+		this.bairo = bairo;
+	}
+}
