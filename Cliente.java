73fa17756fca4eb60a8711133a43d40a38d8e167
Lucas Cardoso dos Santos <31657716+POOLucasCardoso@users.noreply.github.com>
2017-09-19 20:55:10 -0300

    Add files via upload

---
+++ Cliente.java
@@ -0,0 +1,23 @@
+package POO;
+
+public class Cliente extends Pessoa {
+
+	boolean ehInadinplente;
+
+	public Cliente(Contato contatos, Endereco endereco, String nome, String cPF) {
+
+		super(contatos, endereco, nome, cPF);
+
+		this.ehInadinplente = true;
+
+	}
+
+	public boolean isEhInadinplente() {
+		return ehInadinplente;
+	}
+
+	public void setEhInadinplente(boolean ehInadinplente) {
+		this.ehInadinplente = ehInadinplente;
+	}
+
+}
