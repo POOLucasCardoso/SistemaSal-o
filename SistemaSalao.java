88055c1dc4b34bf71b5fa6ab3750578fb1059a46
Lucas Cardoso dos Santos <31657716+POOLucasCardoso@users.noreply.github.com>
2017-10-08 10:42:25 -0300

    Add files via upload

--- SistemaSalao.java
+++ SistemaSalao.java
@@ -1,17 +1,132 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.util.LinkedList;
 public class SistemaSalao {
 	private List<Cliente> clientes;
 	private List<Produto> produtos;
-	private List<Funcionario> fucionarios;
+	private List<Funcionario> funcionarios;
 	private List<Servico> servicos;
-	private List<Apontamento> agenda;
+	List<Apontamento> agenda;
+	private Gravador file;
 	
 	public SistemaSalao(){
+		
+		Servico servico;
+		
+		LinkedList<String> temp = new LinkedList<String>();
+		
+		String[] data;
+		
 		this.clientes = new ArrayList<Cliente>();
+		
+		try {
+			
+			temp = file.load("clientes.txt");
+			
+		}catch(Exception e) {}
+		
+		for(String s: temp) {
+		
+			data = s.split("$");
+			
+			clientes.add(new Cliente(new Contato(data[0],data[1]),new Endereco(data[2],data[3],data[4],data[5],data[6],data[7],data[8]),data[9],data[10]));
+		
+		}
+		
 		this.produtos = new ArrayList<Produto>();
-		this.fucionarios = new ArrayList<Funcionario>();
+		
+		try {
+			
+			temp = file.load("produtos.txt");
+			
+		}catch(Exception e) {}
+		
+		for(String s: temp) {
+			
+			data = s.split("$");
+			
+			produtos.add(new Produto(data[0],data[1],data[2],Double.parseDouble(data[3])));
+			
+		}
+		
+		this.funcionarios = new ArrayList<Funcionario>();
+
+		try {
+			
+			temp = file.load("funcionarios.txt");
+			
+		}catch(Exception e) {}
+		
+		for(String s: temp) {
+			
+			data = s.split("$");
+			
+			funcionarios.add(new Funcionario(data[0],data[1],data[2],new Contato(data[3],data[4]),new Endereco(data[5],data[6],data[7],data[8],data[9],data[10],data[11])));
+			
+		}
+		
+		this.servicos = new LinkedList<Servico>();
+
+		try {
+			
+			temp = file.load("servicos.txt");
+			
+		}catch(Exception e) {}
+
+		for(String s: temp) {
+			
+			data = s.split("$");
+			
+			servico = new Servico(data[0],Double.parseDouble(data[1]));
+			
+			Produto produto;
+			
+			for(int i = 2; i<data.length; i+=2) {
+				
+				try{
+					
+					produto = this.pesquisaProduto(data[i], data[i+1]);
+					
+					servico.addProduto(produto);
+					
+				}catch(Exception e){};
+				
+			}
+			
+			servicos.add(servico);
+			
+		}
+		
 		this.agenda = new ArrayList<Apontamento>();
+
+		try {
+			
+			temp = file.load("agenda.txt");
+			
+		}catch(Exception e) {}
+		
+		Cliente cliente;
+		
+		Funcionario funcionario;
+
+		for(String s: temp) {
+			
+			data = s.split("$");
+			
+			try {
+				
+				cliente = this.pesquisaCliente(data[0]);
+				
+				funcionario = this.pesquisaFuncionario(data[1]);
+				
+				servico = this.pesquisaServico(data[2]);
+				
+				agenda.add(new Apontamento(data[3],data[4],cliente,funcionario,servico));
+				
+			}catch(Exception e){};
+			
+		}
+		
 	}
 	
 	public int quantClientes(){
@@ -23,13 +138,13 @@ public class SistemaSalao {
 	}
 	
 	public int quantFuncionarios(){
-		return this.fucionarios.size();
+		return this.funcionarios.size();
 	}
 	
 	public List<Cliente> quantClientesInad(){
 		List<Cliente> clientesInadiplente = new ArrayList<Cliente>();
 		for(int k = 0; k < this.clientes.size(); k++){
-			if(this.clientes.get(k).isEhInadinplente()){
+			if(this.clientes.get(k).isEhInadimplente()){
 				clientesInadiplente.add(this.clientes.get(k));
 			}	
 		}
@@ -64,8 +179,8 @@ public class SistemaSalao {
 	
 	public List<Funcionario> todosfuncionarios(){
 		List<Funcionario> funcionarios = new ArrayList<Funcionario>();
-		for(int k = 0; k < this.fucionarios.size(); k++){
-			funcionarios.add(this.fucionarios.get(k));
+		for(int k = 0; k < this.funcionarios.size(); k++){
+			funcionarios.add(this.funcionarios.get(k));
 		}
 		return funcionarios;
 	}
@@ -89,12 +204,12 @@ public class SistemaSalao {
 	}
 	
 	public void cadastraFuncionario(Funcionario funcionario) throws JaExisteException{
-		for(int k = 0; k < this.fucionarios.size(); k++){
-			if(this.fucionarios.get(k).equals(funcionario)){
+		for(int k = 0; k < this.funcionarios.size(); k++){
+			if(this.funcionarios.get(k).equals(funcionario)){
 				throw new JaExisteException("Funcionario Já Cadastrado");
 			}
 		}
-		this.fucionarios.add(funcionario);
+		this.funcionarios.add(funcionario);
 	}
 	
 	public Cliente pesquisaCliente(String cpf) throws NaoExisteException{
@@ -107,9 +222,9 @@ public class SistemaSalao {
 	}
 	
 	public Funcionario pesquisaFuncionario(String cpf)throws NaoExisteException{
-		for(int k = 0; k<this.fucionarios.size(); k++){
-			if(this.fucionarios.get(k).getCPF().equals(cpf)){
-				return this.fucionarios.get(k);
+		for(int k = 0; k<this.funcionarios.size(); k++){
+			if(this.funcionarios.get(k).getCPF().equals(cpf)){
+				return this.funcionarios.get(k);
 			}
 		}
 		throw new NaoExisteException("Não Existe");
@@ -154,7 +269,7 @@ public class SistemaSalao {
 
 		boolean boo = true;
 
-		for (Funcionario f : this.fucionarios) {
+		for (Funcionario f : this.funcionarios) {
 
 			if (f.getCPF().equals(CPF)) {
 
@@ -299,6 +414,126 @@ public class SistemaSalao {
 
 		}
 
+	}
+	
+	public void finaliza() {
+		
+		String texto = "";
+		
+		LinkedList<String> clientes = new LinkedList<String>();
+		
+		for(Cliente c: this.clientes) {
+			
+			texto += c.getContatos().getEmail()+"$";
+			texto += c.getContatos().getNumero()+"$";
+			texto += c.getEndereco().getPais()+"$";
+			texto += c.getEndereco().getEstado()+"$";
+			texto += c.getEndereco().getCidade()+"$";
+			texto += c.getEndereco().getBairo()+"$";
+			texto += c.getEndereco().getRua()+"$";
+			texto += c.getEndereco().getNumero()+"$";
+			texto += c.getEndereco().getCEP()+"$";
+			texto += c.getNome()+"$";
+			texto += c.getCPF()+"$";
+			
+			if (c.isEhInadimplente()) {
+				
+				texto+="T";
+				
+			}else {
+				
+				texto+="F";
+				
+			}
+			
+			clientes.add(texto);
+			
+		}
+		
+		LinkedList<String> funcionarios = new LinkedList<String>();
+		
+		texto = "";
+		
+		for(Funcionario f: this.funcionarios) {
+			
+			texto += f.getTipo();
+			texto += f.getNome()+"$";
+			texto += f.getCPF()+"$";
+			texto += f.getContatos().getEmail()+"$";
+			texto += f.getContatos().getNumero()+"$";
+			texto += f.getEndereco().getPais()+"$";
+			texto += f.getEndereco().getEstado()+"$";
+			texto += f.getEndereco().getCidade()+"$";
+			texto += f.getEndereco().getBairo()+"$";
+			texto += f.getEndereco().getRua()+"$";
+			texto += f.getEndereco().getNumero()+"$";
+			texto += f.getEndereco().getCEP()+"$";
+			
+			funcionarios.add(texto);
+			
+		}
+		
+		LinkedList<String> produtos = new LinkedList<String>();
+		
+		texto = "";
+		
+		for(Produto p: this.produtos) {
+			
+			texto += p.getMarca()+"$";
+			texto += p.getTipo()+"$";
+			texto += p.getNome()+"$";
+			texto += p.getPreco();
+			
+			produtos.add(texto);
+			
+		}
+		
+		LinkedList<String> servicos = new LinkedList<String>();
+		
+		texto = "";
+		
+		for (Servico s: this.servicos) {
+			
+			texto += s.getTipo()+"$";
+			texto += s.getPreco()+"$";
+			
+			for (Produto p: s.getProdutosNecessario()) {
+				
+				texto += p.getNome()+"$";
+				texto += p.getMarca()+"$";
+				
+			}
+			
+			servicos.add(texto);
+			
+		}
+		
+		LinkedList<String> agenda = new LinkedList<String>();
+		
+		texto = "";
+		
+		for (Apontamento a: this.agenda) {
+			
+			texto += a.getCliente().getCPF()+"$";
+			texto += a.getFuncionario().getCPF()+"$";
+			texto += a.getServico().getTipo()+"$";
+			texto += a.getData()+"$";
+			texto += a.getHora()+"$";
+			
+			agenda.add(texto);
+			
+		}
+		
+		try {
+		
+		file.save(clientes, "clientes.txt");
+		file.save(funcionarios, "funcionarios.txt");
+		file.save(produtos, "produtos.txt");
+		file.save(servicos, "servicos.txt");
+		file.save(agenda, "agenda.txt");
+		
+		} catch(Exception e) {}
+		
 	}
 	
 }
